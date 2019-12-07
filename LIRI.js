a9075115a5964bbb216c15a61f03aaea7941c2e6
Lucas Aho <aholucas11@gmail.com>
2019-12-07 02:25:38 -0700

    Terminal now takes 4 commands and logs a response to each. Instructions also placed

--- LIRI.js
+++ LIRI.js
@@ -10,3 +10,32 @@
 //Turned in as CLI app
     //Screenshots/GIF/and/or video format showing working app
         //link to ^ in README
+        
+// require("dotenv").config();
+// var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
+// spotify-this-song;
+
+//user type concert-this <artist/band name here>
+    //maybe if arg2 = command
+
+var cmdIn  = process.argv[2];
+var srchIn = process.argv[3];
+
+var concertSearch = "concert-this";
+var spotifySearch = "spotify-this-song";
+var movieSearch = "movie-this";
+var randomPick = "do-what-it-says";
+
+
+if (cmdIn === concertSearch) {
+    console.log("No bands");
+} else if (cmdIn === spotifySearch) {
+    console.log("Songs are illegal")
+} else if (cmdIn === movieSearch) {
+    console.log("These movies are censored")
+} else if (cmdIn === randomPick) {
+    console.log("Good. You are under my control");
+} else {
+    console.log("Bruh, you gotta pick one");
+}
