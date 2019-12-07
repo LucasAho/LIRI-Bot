0e8c3d239b3a4b62bf8f98f39c13e25e9c4d1d06
Lucas Aho <aholucas11@gmail.com>
2019-12-06 23:07:41 -0700

    LIRI.js and .gitignore added, JSON package downloaded, instructions added

---
+++ LIRI.js
@@ -0,0 +1,12 @@
+//Search spotify for songs, bands in town for concerts, and OMDB for movies
+    //api reader with axios packages
+        //https://www.npmjs.com/package/node-spotify-api
+        //https://www.npmjs.com/package/axios
+        //http://www.omdbapi.com/
+        //http://www.artists.bandsintown.com/bandsintown-api
+        //https://www.npmjs.com/package/moment
+        //https://www.npmjs.com/package/dotenv
+
+//Turned in as CLI app
+    //Screenshots/GIF/and/or video format showing working app
+        //link to ^ in README
